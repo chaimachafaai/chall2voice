06125c0b669e445eaa0af8c3ad356eb540f115aa
chaima chafaai <chafaaichaimaait@gmail.com>
2025-11-09 21:23:26 +0100

    Initial Commit

---
+++ challenge 2/challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_2App.swift
+//  challenge 2
+//
+//  Created by Chaima Ait Chafaai on 09/11/25.
+//
+
+import SwiftUI
+
+@main
+struct challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
